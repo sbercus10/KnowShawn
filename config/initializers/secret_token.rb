d712c808ac3711a263df8aac89f89c65a11e7bea
Shawn Bercuson <shawn.bercuson@gmail.com>
2014-01-23 19:57:43 -0700

    Initial Rails Knowshawn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knowshawn::Application.config.secret_token = '159c3116b2f8bcb8f05c8b2f79f68d8029d988d50352461c0df70c0465f6c5ec7558430c00505fab568afef744801cbcea22cd4213930908067f47233c90dee7'
